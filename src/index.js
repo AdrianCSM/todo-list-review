49b30e0cc73898684d2287bef0e2c9ced209acab
Cosmin Adrian <cosminadrian218@gmail.com>
2022-03-29 17:15:19 +0300

    Fix linters and webpack

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const tasks = new List();
 document.querySelector('#add-task').addEventListener('submit', (e) => {
   e.preventDefault();
   const activity = e.target.elements.activity.value;
-  // tasks.addActivity(activity);
+  tasks.addActivity(activity);
   // e.target.reset();
 });
 
